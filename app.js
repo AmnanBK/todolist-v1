de12eb506786e153c6ecf3e968990eaa93a20cf4
Amnan <amnanbudikusuma@gmail.com>
2023-03-02 23:23:03 +0700

    add Styling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 var items = []
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
 app.set("view engine", "ejs")
 
 app.get("/", function (req, res) {

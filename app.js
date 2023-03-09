3f01a8016b5531dc271434dd2edf6b81a5a3d7ec
Amnan <amnanbudikusuma@gmail.com>
2023-03-09 10:27:15 +0700

    added database to / path

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 const logic = require(__dirname + "/logic.js")
 
+mongoose.connect("mongodb://127.0.0.1:27017/toDoDB")
 const app = express()
-let items = []
+
+const Activities = mongoose.model("Activities", {name: String})
+
 let workItems = []
 let pathNow = ["/", "/work"]
 let date = logic.getDate()
 let titleList = [date, "Work"]
+
+function getName(data) {
+    return data.name
+}
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
@@ -15,19 +23,34 @@ app.use(express.static("public"))
 app.set("view engine", "ejs")
 
 app.get("/", function (req, res) {
-    res.render("list", { titleList: titleList[0], items: items, actionValue: pathNow[0] })
+    let items = []
+    Activities.find({}).then((names) => {
+        for(i=0; i<names.length; i++) {
+            items.push(names[i].name)
+        }
+        res.render("list", { titleList: titleList[0], items: items, actionValue: pathNow[0] })
+    })
 })
 
 app.post("/", function(req, res) {
     let action = req.body.action
 
+
     if(action == "submit") {
         let item = req.body.newItem
-        items.push(item)
+        const activity = new Activities ({ name: item})
+        activity.save().then(() => console.log(item))
+
+        // Activities.find().then((obj) => {
+        //     items.push(obj[obj.length-1].name)
+        // })
     }
 
     if(action == "reset") {
-        items = []
+        Activities.deleteMany({__v: 0}).then(() => {
+            console.log("Deleted");
+            items = []
+        })
     }
     res.redirect("/")
 })
